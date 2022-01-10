fd3aa478ce178553a727f0402a8fc3c146668e2a
kumarmo2 <kr.mohit6794@gmail.com>
2022-01-11 01:07:44 +0530

    added one unit test.

--- tests/Business/MovieLogic/CreateMovie_Tests.cs
+++ tests/Business/MovieLogic/CreateMovie_Tests.cs
@@ -13,6 +13,7 @@ using BMS.Business.Movie;
 using mm = BMS.Models.Movie;
 using BMS.Tests;
 using BMS.DataAccess.Movie;
+using BMS.Dtos.Movie;
 
 public class CreateMovie_Tests
 {
@@ -22,12 +23,19 @@ public class CreateMovie_Tests
         await Assert.ThrowsAsync<ArgumentNullException>(async () => await sut.CreateMovie(null));
     }
 
-    public async Task Movie_Name_Already_Exists_Returns_Error(Mock<IMovieRepository> movieRepositoryStub, mm.Movie existingMovie, MovieLogic sut)
+    [Theory, AutoDomainData]
+    public async Task Movie_Name_Already_Exists_Returns_Error([Frozen] Mock<IMovieRepository> movieRepositoryStub,
+                mm.Movie existingMovie, CreateMovieRequest request, MovieLogic sut)
     {
-        // movieRepositoryStub.Setup(mr => mr.GetByName(It.IsAny<string>())).ReturnsAsync<mm.Movie>(existingMovie);
-        // movieRepositoryStub.Setup(mr => mr.GetByName(It.IsAny<string>())).ReturnsAsync<mm.Movie>(existingMovie);
+        movieRepositoryStub.Setup(mr => mr.GetByName(It.IsAny<string>())).Returns(Task.FromResult(existingMovie));
 
+        var result = await sut.CreateMovie(request);
 
+        Assert.NotNull(result);
+        Assert.NotNull(result.Err);
+        Assert.Null(result.Ok);
+
+        Assert.Equal("Movie with same name already exists", result.Err);
     }
 }
 
